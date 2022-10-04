78b84f099342c8dade0185eaf6ecb41be9820168
thj0309 <thj0309@hotmail.com>
2022-10-04 15:37:15 +0900

    Spring Cloud Gateway init commit

---
+++ src/main/java/com/gateway/gateway/config/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.gateway.gateway.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthcheck")
+public class HealthCheckController {
+
+    @GetMapping("/check")
+    public String check() {
+        return "check";
+    }
+
+}
